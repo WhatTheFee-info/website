2237ac3b60fd70f87ebda0b17efc1553d081ea35
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2024-01-14 21:32:13 -0300

    fix build

--- src/ui/components/FeeRatePanel.tsx
+++ src/ui/components/FeeRatePanel.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import FeeRateBox from './FeeRateBox';
 import { AppContext } from '../../AppContext';
 
